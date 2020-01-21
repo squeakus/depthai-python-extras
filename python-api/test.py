c114121db72142394d50c54544e6fca65e595641
Taras Chaykivskyy <taras@luxonis.com>
2020-01-21 22:42:05 +0200

    version 0.0.7a

--- python-api/test.py
+++ python-api/test.py
@@ -1,6 +1,6 @@
 import sys
 from time import time
-
+from time import sleep
 import numpy as np
 import cv2
 
@@ -12,22 +12,35 @@ import consts.resource_paths
 print('depthai.__version__ == %s' % depthai.__version__)
 
 
-cmd_file = consts.resource_paths.device_depth_cmd_fpath
+cmd_file = consts.resource_paths.device_ai_cmd_fpath
 if len(sys.argv) > 1 and sys.argv[1] == "debug":
     cmd_file = ''
     print('depthai will not load cmd file into device.')
 
 
-counter = 0
+labels = []
+with open(consts.resource_paths.blob_labels_fpath) as fp:
+    labels = fp.readlines()
+    labels = [i.strip() for i in labels]
 
-streams_list = ['left', 'right', 'depth', 'meta_d2h']
 
-pipieline = depthai.create_pipeline(
+streams_list = ['meta_d2h', 'metaout', 'previewout', 'depth_color_h']
+# streams_list = ['meta_d2h', 'disparity', 'left', 'right', 'metaout', 'previewout', 'depth_mm_h', 'depth_color_h']
+
+
+# create the pipeline, here is the first connection with the device
+p = depthai.create_pipeline(
         streams=streams_list,
         cmd_file=cmd_file,
-        calibration_file=consts.resource_paths.calib_fpath,
-        config_file=consts.resource_paths.pipeline_config_fpath
+        blob_file = consts.resource_paths.blob_fpath,
+        blob_file_config = consts.resource_paths.blob_config_fpath,
+        pipeline_file_config = consts.resource_paths.pipeline_config_fpath,
+        calibration_file = consts.resource_paths.calib_fpath,
         )
+
+if p is None:
+    print('Pipeline is not created.')
+    exit(1)
 
 
 t_start = time()
@@ -37,34 +50,88 @@ for s in streams_list:
     frame_count[s] = 0
     frame_count_prev[s] = 0
 
+entries_prev = []
 
 while True:
-    data_list = pipieline.get_available_data_packets()
+    # retreive data from the device
+    # data is stored in packets, there are nnet (Neural NETwork) packets which have additional functions for NNet result interpretation
+    nnet_packets, data_packets = p.get_available_nnet_and_data_packets()
 
-    for packet in data_list:
-        if packet.stream_name == 'depth':
-            frame_bgr = packet.getData()
-            cv2.putText(frame_bgr, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
-            cv2.imshow(packet.stream_name, frame_bgr)
-        elif packet.stream_name == 'meta_d2h':
+    for i, nnet_packet in enumerate(nnet_packets):
+        # the result of the MobileSSD has detection rectangles (here: entries), and we can iterate threw them
+        for i, e in enumerate(nnet_packet.entries()):
+            # for MobileSSD entries are sorted by confidence
+            # {id == -1} or {confidence == 0} is the stopper (special for OpenVINO models and MobileSSD architecture)
+            if e[0]['id'] == -1.0 or e[0]['confidence'] == 0.0:
+                break
+
+            if i == 0:
+                entries_prev.clear()
+
+            # save entry for further usage (as image package may arrive not the same time as nnet package)
+            entries_prev.append(e)
+
+    for packet in data_packets:
+        if packet.stream_name == 'previewout':
             data = packet.getData()
-            print('meta_d2h counter: ' + str(data[0]))
+            # the format of previewout image is CHW (Chanel, Height, Width), but OpenCV needs HWC, so we
+            # change shape (3, 300, 300) -> (300, 300, 3)
+            data0 = data[0,:,:]
+            data1 = data[1,:,:]
+            data2 = data[2,:,:]
+            frame = cv2.merge([data0, data1, data2])
 
-        elif packet.stream_name == 'left':
+            img_h = frame.shape[0]
+            img_w = frame.shape[1]
+
+            # iterate threw pre-saved entries & draw rectangle & text on image:
+            for e in entries_prev:
+                # the lower confidence threshold - the more we get false positives
+                if e[0]['confidence'] > 0.5:
+                    x1 = int(e[0]['left'] * img_w)
+                    y1 = int(e[0]['top'] * img_h)
+
+                    pt1 = x1, y1
+                    pt2 = int(e[0]['right'] * img_w), int(e[0]['bottom'] * img_h)
+
+                    cv2.rectangle(frame, pt1, pt2, (0, 0, 255))
+
+                    # Handles case where TensorEntry object label = 7552.
+                    if e[0]['label'] > len(labels):
+                        print("Label index=",e[0]['label'], "is out of range. Not applying text to rectangle.")
+                    else:
+                        pt_t1 = x1, y1 + 20
+                        cv2.putText(frame, labels[int(e[0]['label'])], pt_t1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+                        pt_t2 = x1, y1 + 40
+                        cv2.putText(frame, str(e[0]['confidence']), pt_t2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+            cv2.putText(frame, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
+            cv2.imshow('previewout', frame)
+        elif packet.stream_name == 'left' or packet.stream_name == 'right' or packet.stream_name == 'disparity':
             frame_bgr = packet.getData()
-            cv2.putText(frame_bgr, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
+            cv2.putText(frame_bgr, packet.stream_name, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
+            cv2.putText(frame_bgr, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
             cv2.imshow(packet.stream_name, frame_bgr)
-        elif packet.stream_name == 'right':
-            frame_bgr = packet.getData()
-            cv2.putText(frame_bgr, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
-            cv2.imshow(packet.stream_name, frame_bgr)
-        else:
-            print('Packet : ' + packet.stream_name)
+        elif packet.stream_name.startswith('depth'):
+            frame = packet.getData()
+
+            if len(frame.shape) == 2:
+                if frame.dtype == np.uint8: # grayscale
+                    cv2.putText(frame, packet.stream_name, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
+                    cv2.putText(frame, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
+                    cv2.imshow(packet.stream_name, frame)
+                else: # uint16
+                    frame_u8 = (frame // 255).astype(np.uint8)
+                    cv2.putText(frame_u8, packet.stream_name, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, 255)
+                    cv2.putText(frame_u8, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, 255)
+                    cv2.imshow(packet.stream_name, frame_u8)
+            else: # bgr
+                cv2.putText(frame, packet.stream_name, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+                cv2.putText(frame, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, 255)
+                cv2.imshow(packet.stream_name, frame)
 
         frame_count[packet.stream_name] += 1
-
-    if cv2.waitKey(1) == ord('q'):
-        break
 
     t_curr = time()
     if t_start + 1.0 < t_curr:
@@ -73,5 +140,10 @@ while True:
         for s in streams_list:
             frame_count_prev[s] = frame_count[s]
             frame_count[s] = 0
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+del p  # in order to stop the pipeline object should be deleted, otherwise device will continue working. This is required if you are going to add code after the main loop, otherwise you can ommit it.
 
 print('py: DONE.')
